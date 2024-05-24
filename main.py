b63efe492b659a862ed04201cfdb7e35e5e8be9c
jiaebaek <167492751+jiaebaek@users.noreply.github.com>
2024-05-24 15:25:00 +0900

    using pykiwoom

--- main.py
+++ main.py
@@ -1,76 +1,101 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, QWidget
-from KiwoomAPI import KiwoomAPI
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QTableWidget, \
+    QTableWidgetItem
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from pykiwoom.kiwoom import Kiwoom
+
 
 class StockTradingApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.kiwoom = KiwoomAPI()
-        self.kiwoom.comm_connect()
-        self.kiwoom.get_account_info()
+
+        # Kiwoom 인스턴스 생성 및 로그인
+        self.kiwoom = Kiwoom()
+        self.kiwoom.CommConnect(block=True)
 
         self.initUI()
-        self.load_stock_data()
-        self.load_unfilled_orders()
+        self.loadData()
 
     def initUI(self):
         self.setWindowTitle('Stock Trading App')
         self.setGeometry(100, 100, 800, 600)
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
 
-        self.layout = QVBoxLayout(self.central_widget)
+        layout = QVBoxLayout()
+        centralWidget.setLayout(layout)
+
+        title = QLabel('My Stock Portfolio')
+        title.setFont(QFont('Arial', 20))
+        title.setAlignment(Qt.AlignCenter)
+        layout.addWidget(title)
 
         # 주식 리스트 테이블
-        self.stock_table = QTableWidget(self)
-        self.layout.addWidget(self.stock_table)
+        self.stockTable = QTableWidget()
+        self.stockTable.setColumnCount(4)
+        self.stockTable.setHorizontalHeaderLabels(['Stock Code', 'Stock Name', 'Quantity', 'Current Price'])
+        layout.addWidget(self.stockTable)
 
         # 미체결 주문 리스트 테이블
-        self.unfilled_orders_table = QTableWidget(self)
-        self.layout.addWidget(self.unfilled_orders_table)
+        self.orderTable = QTableWidget()
+        self.orderTable.setColumnCount(4)
+        self.orderTable.setHorizontalHeaderLabels(['Order Number', 'Stock Code', 'Order Quantity', 'Order Price'])
+        layout.addWidget(self.orderTable)
 
-        # 일괄 매도 버튼
-        self.sell_all_button = QPushButton('일괄 매도', self)
-        self.sell_all_button.clicked.connect(self.sell_all_stocks)
-        self.layout.addWidget(self.sell_all_button)
+        # 매도 버튼
+        sellButton = QPushButton('Sell All Stocks')
+        sellButton.clicked.connect(self.sellAllStocks)
+        layout.addWidget(sellButton)
 
-    def load_stock_data(self):
-        self.kiwoom.get_stock_data()
-        stock_data = self.kiwoom.tr_data['multi']
+    def loadData(self):
+        # 주식 리스트 불러오기
+        account_id = self.kiwoom.GetLoginInfo("ACCNO")[0].strip()
+        stock_list = self.kiwoom.GetStockList(account_id)
 
-        self.stock_table.setRowCount(len(stock_data))
-        self.stock_table.setColumnCount(2)
-        self.stock_table.setHorizontalHeaderLabels(['종목코드', '보유수량'])
+        self.stockTable.setRowCount(len(stock_list))
+        for i, stock in enumerate(stock_list):
+            stock_code = stock['종목번호']
+            stock_name = stock['종목명']
+            quantity = stock['보유수량']
+            current_price = self.kiwoom.GetMasterLastPrice(stock_code)
 
-        for i, row in enumerate(stock_data):
-            for j, item in enumerate(row):
-                self.stock_table.setItem(i, j, QTableWidgetItem(item))
+            self.stockTable.setItem(i, 0, QTableWidgetItem(stock_code))
+            self.stockTable.setItem(i, 1, QTableWidgetItem(stock_name))
+            self.stockTable.setItem(i, 2, QTableWidgetItem(str(quantity)))
+            self.stockTable.setItem(i, 3, QTableWidgetItem(str(current_price)))
 
-    def load_unfilled_orders(self):
-        self.kiwoom.get_unfilled_orders()
-        unfilled_orders_data = self.kiwoom.tr_data['multi']
+        # 미체결 주문 리스트 불러오기
+        order_list = self.kiwoom.GetOrderList(account_id)
 
-        self.unfilled_orders_table.setRowCount(len(unfilled_orders_data))
-        self.unfilled_orders_table.setColumnCount(2)
-        self.unfilled_orders_table.setHorizontalHeaderLabels(['종목코드', '주문수량'])
+        self.orderTable.setRowCount(len(order_list))
+        for i, order in enumerate(order_list):
+            order_no = order['주문번호']
+            stock_code = order['종목번호']
+            order_quantity = order['주문수량']
+            order_price = order['주문가격']
 
-        for i, row in enumerate(unfilled_orders_data):
-            for j, item in enumerate(row):
-                self.unfilled_orders_table.setItem(i, j, QTableWidgetItem(item))
+            self.orderTable.setItem(i, 0, QTableWidgetItem(order_no))
+            self.orderTable.setItem(i, 1, QTableWidgetItem(stock_code))
+            self.orderTable.setItem(i, 2, QTableWidgetItem(str(order_quantity)))
+            self.orderTable.setItem(i, 3, QTableWidgetItem(str(order_price)))
 
-    def sell_all_stocks(self):
-        stock_data = self.kiwoom.tr_data['multi']
+    def sellAllStocks(self):
+        account_id = self.kiwoom.GetLoginInfo("ACCNO")[0].strip()
 
-        for row in stock_data:
-            stock_code = row[0]
-            quantity = int(row[1])
+        for i in range(self.stockTable.rowCount()):
+            stock_code = self.stockTable.item(i, 0).text()
+            quantity = int(self.stockTable.item(i, 2).text())
 
             if quantity > 0:
-                self.kiwoom.send_sell_order(stock_code, quantity)
+                self.kiwoom.SendOrder("Sell All", account_id, 2, stock_code, quantity, 0, "03")
+
+        self.loadData()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = StockTradingApp()
-    ex.show()
+    mainWindow = StockTradingApp()
+    mainWindow.show()
     sys.exit(app.exec_())
